bfa37964a4c5d8e84907e24745c3e3f7d4069cf7
Philipp Tempel <mail@philipptempel.me>
2015-05-10 14:47:52 +0200

    Add `help` support for mcols and nrows

--- helpers/mcols.m
+++ helpers/mcols.m
@@ -1,7 +1,30 @@
 function [m] = mcols(A)
+% MCOLS(A) gets the number of columns of A
+%   
+%   Inputs:
+%   
+%   A: Matrix or vector to count the columns of
+% 
+%   Outputs:
+% 
+%   M: Number of columns of A
+% 
+% 
+% Author: Philipp Tempel <philipp.tempel@isw.uni-stuttgart.de>
+% Date: 2015-05-10
+% Changelog:
+%   2015-05-10: Update help documentation and add support for variables of type
+%               2d
+%   2015-04-27: Initial release
 
-validateattributes(A, {'numeric'}, {'row'}, mfilename, 'A');
+validateattributes(A, {'numeric'}, {'2d'}, mfilename, 'A');
 
 m = size(A, 2);
 
 end
+
+%------------- END OF CODE --------------
+% Please send suggestions for improvement of this file to the original
+% author as can be found in the header
+% Your contribution towards improving this funciton will be acknowledged in
+% the "Changes" section of the header
--- helpers/nrows.m
+++ helpers/nrows.m
@@ -1,7 +1,30 @@
-function [n] = nrows(A)
+function [m] = nrows(A)
+% NROWS(A) gets the number of rows of A
+%   
+%   Inputs:
+%   
+%   A: Matrix or vector to count the rows of
+% 
+%   Outputs:
+% 
+%   M: Number of rows of A
+% 
+% 
+% Author: Philipp Tempel <philipp.tempel@isw.uni-stuttgart.de>
+% Date: 2015-05-10
+% Changelog:
+%   2015-05-10: Update help documentation and add support for variables of type
+%               2d
+%   2015-04-27: Initial release
 
-validateattributes(A, {'numeric'}, {'column'}, mfilename, 'A');
+validateattributes(A, {'numeric'}, {'2d'}, mfilename, 'A');
 
-n = size(A, 1);
+m = size(A, 1);
 
 end
+
+%------------- END OF CODE --------------
+% Please send suggestions for improvement of this file to the original
+% author as can be found in the header
+% Your contribution towards improving this funciton will be acknowledged in
+% the "Changes" section of the header
