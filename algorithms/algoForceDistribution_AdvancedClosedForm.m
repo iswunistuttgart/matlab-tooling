f29c196457191ad44d62d77c71f8a235212f7551
Philipp Tempel <mail@philipptempel.me>
2015-06-12 16:54:17 +0200

    Update force distribution algorithms with proper help documentation

--- algorithms/algoForceDistribution_AdvancedClosedForm.m
+++ algorithms/algoForceDistribution_AdvancedClosedForm.m
@@ -1,52 +1,46 @@
 function [Distribution, varargout] = algoForceDistribution_AdvancedClosedForm(Wrench, StructureMatrix, ForceMinimum, ForceMaximum)
-% ALGOFORCEDISTRIBUTION_CLOSEDFORM - Determine the force distribution for
-%   the given robot using the closed-form force distribution algorithm
+% ALGOFORCEDISTRIBUTION_ADVANCEDCLOSEDFORM - Determine the force distribution
+%   for the given robot using the closed-form force distribution algorithm
 % 
-%   LENGTH = ALGOINVERSEKINEMATICS_SIMPLE(POSE, WINCHPOSITIONS, CABLEATTACHMENTS)
-%   performs simple inverse kinematics with the cables running from a_i to
-%   b_i for the given pose
-% 
-%   [LENGTH, CABLEVECTORS] = ALGOINVERSEKINEMATICS_SIMPLE(...) also provides the
-%   vectors of the cable directions from platform to attachment point given
-%   in the global coordinate system
-% 
-%   [LENGTH, CABLEVECTORS, CABLEUNITVECTORS] = ALGOINVERSEKINEMATICS_SIMPLE(...)
-%   also provides the unit vectors for each cable which might come in handy
-%   at times
+%   DISTRIBUTION = ALGOFORCEDISTRIBUTION_ADVANCEDCLOSEDFORM(WRENCH,
+%   STRUCTUREMATRIX, FORCEMINIMU, FORCEMAXIMUM) calculates the closed-form force
+%   distribution for the given wrench and the pre-calculated structure matrix
 %   
 %   Inputs:
 %   
-%   POSE: The current robots pose given as a 12-column row vector that has
-%   the [x, y, z] position in the first three entries and then follwing are
-%   the entries of the rotation matrix such that the vector POSE looks
-%   something like this
-%   pose = [x, y, z, R11, R12, R13, R21, R22, R23, R31, R32, R33]
-% 
-%   WINCHPOSITIONS: Matrix of winch positions w.r.t. the world frame. Each
-%   winch has its own column and the rows are the x, y, and z-value,
-%   respectively i.e., WINCHPOSITIONS must be a matrix of 3xM values. The
-%   number of winches i.e., N, must match the number of cable attachment
-%   points in CABLEATTACHMENTS (i.e., its column count) and the order must
-%   mach the real linkage of winch to cable attachment on the platform
-% 
-%   CABLEATTACHMENTS: Matrix of cable attachment points w.r.t. the
-%   platforms coordinate system. Each attachment point has its own column
-%   and the rows are the x, y, and z-value, respectively, i.e.,
-%   CABLEATTACHMENTS must be a matrix of 3xM values. The number of cables
-%   i.e., N, must match the number of winches in WINCHPOSITIONS (i.e., its
-%   column count) and the order must match the real linkage of cable
-%   attachment on the platform to winch.
+%   WRENCH: Column-vector of the wrench on the system. Preferably should be a
+%   6x1 vector, but if you know what you are doing, it might work with other
+%   dimensions, too. Generally, 6x1 vectors will work fine, too, as long as you
+%   adjust it properly to your cable robot design
+%   
+%   STRUCTUREMATRIX: The structure matrix At, which must be calculated
+%   beforehand, for which to determine the closed-form force distribution as
+%   given by A. Pott.
+%   
+%   FORCEMINIMUM: Minimum force as required for the algorithm to work. Must
+%   be either a scalar which is then being translated as the minimum for all
+%   cables, or a column vector that has the same number of rows as
+%   STRUCTUREMATRIX has columns
+%   
+%   FORCEMAXIMUM: Maximum force as required for the algorithm to work. Must
+%   be either a scalar which is then being translated as the minimum for all
+%   cables, or a column vector that has the same number of rows as
+%   STRUCTUREMATRIX has columns
 % 
 %   Outputs:
 % 
 %   DISTRIBUTION: Vector of force distribution values as determined by the
 %   algorithm
-% 
+%
 % Author: Philipp Tempel <philipp.tempel@isw.uni-stuttgart.de>
-% Date: 2015-04-22
+% Date: 2015-06-12
 % Changelog:
-%   2015-04-22: Initial release
-
+%   2015-06-12:
+%       * Make sure that vForce{Max,Min}imum are column vectors
+%       * Fix typos in comments and variable names
+%       * Finally write method help documentation
+%   2015-04-22:
+%       * Initial release
 
 
 %% Assert and parse variables
@@ -71,6 +65,9 @@ else
 end
 % Vector of mean force values
 vForceMean = 0.5.*(vForceMinimum + vForceMaximum);
+
+% Initialize the force distribution holding vector
+vForceDistribution = zeros(iNumberOfWires, 1);
 
 
 
--- algorithms/algoForceDistribution_ClosedForm.m
+++ algorithms/algoForceDistribution_ClosedForm.m
@@ -2,40 +2,30 @@ function [Distribution, varargout] = algoForceDistribution_ClosedForm(Wrench, St
 % ALGOFORCEDISTRIBUTION_CLOSEDFORM - Determine the force distribution for
 %   the given robot using the closed-form force distribution algorithm
 % 
-%   LENGTH = ALGOINVERSEKINEMATICS_SIMPLE(POSE, WINCHPOSITIONS, CABLEATTACHMENTS)
-%   performs simple inverse kinematics with the cables running from a_i to
-%   b_i for the given pose
-% 
-%   [LENGTH, CABLEVECTORS] = ALGOINVERSEKINEMATICS_SIMPLE(...) also provides the
-%   vectors of the cable directions from platform to attachment point given
-%   in the global coordinate system
-% 
-%   [LENGTH, CABLEVECTORS, CABLEUNITVECTORS] = ALGOINVERSEKINEMATICS_SIMPLE(...)
-%   also provides the unit vectors for each cable which might come in handy
-%   at times
+%   DISTRIBUTION = ALGOFORCEDISTRIBUTION_CLOSEDFORM(WRENCH, STRUCTUREMATRIX,
+%   FORCEMINIMU, FORCEMAXIMUM) calculates the closed-form force distribution for
+%   the given wrench and the pre-calculated structure matrix
 %   
 %   Inputs:
 %   
-%   POSE: The current robots pose given as a 12-column row vector that has
-%   the [x, y, z] position in the first three entries and then follwing are
-%   the entries of the rotation matrix such that the vector POSE looks
-%   something like this
-%   pose = [x, y, z, R11, R12, R13, R21, R22, R23, R31, R32, R33]
-% 
-%   WINCHPOSITIONS: Matrix of winch positions w.r.t. the world frame. Each
-%   winch has its own column and the rows are the x, y, and z-value,
-%   respectively i.e., WINCHPOSITIONS must be a matrix of 3xM values. The
-%   number of winches i.e., N, must match the number of cable attachment
-%   points in CABLEATTACHMENTS (i.e., its column count) and the order must
-%   mach the real linkage of winch to cable attachment on the platform
-% 
-%   CABLEATTACHMENTS: Matrix of cable attachment points w.r.t. the
-%   platforms coordinate system. Each attachment point has its own column
-%   and the rows are the x, y, and z-value, respectively, i.e.,
-%   CABLEATTACHMENTS must be a matrix of 3xM values. The number of cables
-%   i.e., N, must match the number of winches in WINCHPOSITIONS (i.e., its
-%   column count) and the order must match the real linkage of cable
-%   attachment on the platform to winch.
+%   WRENCH: Column-vector of the wrench on the system. Preferably should be a
+%   6x1 vector, but if you know what you are doing, it might work with other
+%   dimensions, too. Generally, 6x1 vectors will work fine, too, as long as you
+%   adjust it properly to your cable robot design
+%   
+%   STRUCTUREMATRIX: The structure matrix At, which must be calculated
+%   beforehand, for which to determine the closed-form force distribution as
+%   given by A. Pott.
+%   
+%   FORCEMINIMUM: Minimum force as required for the algorithm to work. Must
+%   be either a scalar which is then being translated as the minimum for all
+%   cables, or a column vector that has the same number of rows as
+%   STRUCTUREMATRIX has columns
+%   
+%   FORCEMAXIMUM: Maximum force as required for the algorithm to work. Must
+%   be either a scalar which is then being translated as the minimum for all
+%   cables, or a column vector that has the same number of rows as
+%   STRUCTUREMATRIX has columns
 % 
 %   Outputs:
 % 
@@ -43,9 +33,14 @@ function [Distribution, varargout] = algoForceDistribution_ClosedForm(Wrench, St
 %   algorithm
 %
 % Author: Philipp Tempel <philipp.tempel@isw.uni-stuttgart.de>
-% Date: 2015-04-22
+% Date: 2015-06-12
 % Changelog:
-%   2015-04-22: Initial release
+%   2015-06-12:
+%       * Make sure that vForce{Max,Min}imum are column vectors
+%       * Fix typos in comments and variable names
+%       * Finally write method help documentation
+%   2015-04-22:
+%       * Initial release
 
 
 
@@ -61,13 +56,13 @@ iNumberOfWires = size(mStructureMatrixAt, 2);
 if isscalar(ForceMinimum)
     vForceMinimum = ForceMinimum.*ones(iNumberOfWires, 1);
 else
-    vForceMinimum = ForceMinimum;
+    vForceMinimum = ForceMinimum(:);
 end
 % Force maximum, can be given a scalar or a vector
 if isscalar(ForceMaximum)
     vForceMaximum = ForceMaximum.*ones(iNumberOfWires, 1);
 else
-    vForceMaximum = ForceMaximum;
+    vForceMaximum = ForceMaximum(:);
 end
 % Vector of mean force values
 vForceMean = 0.5.*(vForceMinimum + vForceMaximum);
@@ -81,11 +76,11 @@ if issquare(mStructureMatrixAt)
     mForceDistribution = mStructureMatrixAt\(-vWrench);
 % Non standard case, where we have more cables than degrees of freedom
 else
-    % Solve A^t f_v & = - ( w + A^t f_m )
+    % Solve A^t f_v = - ( w + A^t f_m )
     % Determine the pseudo inverse of A^t
-    mStructureMatrixPseudeoInverse = transpose(mStructureMatrixAt)/(mStructureMatrixAt*transpose(mStructureMatrixAt));
+    mStructureMatrixPseudoInverse = transpose(mStructureMatrixAt)/(mStructureMatrixAt*transpose(mStructureMatrixAt));
     % And determine the force distribution
-    mForceDistribution = vForceMean - mStructureMatrixPseudeoInverse*(vWrench + mStructureMatrixAt*vForceMean);
+    mForceDistribution = vForceMean - mStructureMatrixPseudoInverse*(vWrench + mStructureMatrixAt*vForceMean);
 end
 
 
