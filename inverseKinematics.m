b0be81e5e400074f170bfab615eb3b58b824289d
Philipp Tempel <mail@philipptempel.me>
2015-06-15 18:17:01 +0200

    Change 'Winch' to 'Pulley' in inverseKinematics function
    Also add support and documentation for more inverse kinematics algorithms

--- inverseKinematics.m
+++ inverseKinematics.m
@@ -1,4 +1,4 @@
-function [length, varargout] = inverseKinematics(Pose, WinchPositions, CableAttachments, varargin)
+function [length, varargout] = inverseKinematics(Pose, PulleyPositions, CableAttachments, varargin)
 % INVERSEKINEMATICS - Perform inverse kinematics for the given robot
 %   Inverse kinematics means to determine the values for the joint
 %   variables (in this case cable lengths) for a given endeffector pose.
@@ -8,23 +8,34 @@ function [length, varargout] = inverseKinematics(Pose, WinchPositions, CableAtta
 %   It can determine the cable lengths for any given robot configuration
 %   (note that calculations will be done as if we were looking at a 3D/6DOF
 %   cable robot following necessary conventions, so adjust your variables
-%   accordingly). To determine the cable lengths, both the simple kinematic
-%   loop can be used as well as the advanced pulley kinematics (considering
-%   pulley radius and rotatability).
+%   accordingly). To determine the cable lengths, different algorithms may be
+%   used (please see below for the 'ALGORITHM' option). Note that all cable
+%   lengths will always be from the PulleyPosition to the platform, any free
+%   cable length that you might have before the last pulley needs to be added by
+%   you manually after running this method
 % 
-%   LENGTH = INVERSEKINEMATICS(POSE, WINCHPOSITIONS, CABLEATTACHMENTS)
+%   LENGTH = INVERSEKINEMATICS(POSE, PULLEYPOSITIONS, CABLEATTACHMENTS)
 %   performs simple inverse kinematics with the cables running from a_i to
 %   b_i for the given pose
 %   
-%   LENGTH = INVERSEKINEMATICS(POSE, WINCHPOSITIONS, CABLEATTACHMENTS, true)
-%   performs advanced inverse kinematics for the given pose assuming no
-%   separate rotation of the winches nor any pulley radius
+%   LENGTH = INVERSEKINEMATICS(POSE, PULLEYPOSITIONS, CABLEATTACHMENTS,
+%   ALGORITHM) performs inverse kinematics according to the selected algorithm.
+%   Implemented algorithms are
+%       'Simple'            Standard straight-line model
+%       'Pulley'            Pulley kinematics with pulley radius and rotation
+%                           included
+%       'CatenarySimple'    TO COME Using a catenary approach the cable length will be
+%                           calculated assuming ideal cable entry points (i.e.,
+%                           no rotation or radius of the pulleys.
+%       'CatenaryPulley'    TO COME Most advanced algorithm to determine catenary cable
+%                           line with the inclusion of pulleys that is rotation
+%                           of the pulley and cable wrapping around the pulley.
 %   
-%   LENGTH = INVERSEKINEMATICS(POSE, ..., 'WinchOrientations', mWinchOrientations)
-%   performs advanced inverse kinematics given the specified winch
-%   orientations.
+%   LENGTH = INVERSEKINEMATICS(POSE, ..., 'PulleyOrientations',
+%   mPulleyOrientations) performs non-standard inverse kinematics given the
+%   specified pulley orientations.
 %   
-%   LENGTH = INVERSEKINEMATICS(POSE, ..., 'WinchPulleyRadius', vPulleyRadius)
+%   LENGTH = INVERSEKINEMATICS(POSE, ..., 'PulleyRadius', vPulleyRadius)
 %   performs advanced inverse kinematics taking into account the specified
 %   pulley radius.
 % 
@@ -44,36 +55,36 @@ function [length, varargout] = inverseKinematics(Pose, WinchPositions, CableAtta
 %   something like this
 %   pose = [x, y, z, R11, R12, R13, R21, R22, R23, R31, R32, R33]
 % 
-%   WINCHPOSITIONS: Matrix of winch positions w.r.t. the world frame. Each
-%   winch has its own column and the rows are the x, y, and z-value,
-%   respectively i.e., WINCHPOSITIONS must be a matrix of 3xM values. The
-%   number of winches i.e., M, must match the number of cable attachment
+%   PULLEYPOSITIONS: Matrix of pulley positions w.r.t. the world frame. Each
+%   pulley has its own column and the rows are the x, y, and z-value,
+%   respectively i.e., PULLEYPOSITIONS must be a matrix of 3xM values. The
+%   number of pulleyes i.e., M, must match the number of cable attachment
 %   points in CABLEATTACHMENTS (i.e., its column count) and the order must
-%   mach the real linkage of winch to cable attachment on the platform
+%   mach the real linkage of pulley to cable attachment on the platform
 % 
 %   CABLEATTACHMENTS: Matrix of cable attachment points w.r.t. the
 %   platforms coordinate system. Each attachment point has its own column
 %   and the rows are the x, y, and z-value, respectively, i.e.,
 %   CABLEATTACHMENTS must be a matrix of 3xM values. The number of cables
-%   i.e., M, must match the number of winches in WINCHPOSITIONS (i.e., its
+%   i.e., M, must match the number of pulleyes in PULLEYPOSITIONS (i.e., its
 %   column count) and the order must match the real linkage of cable
-%   attachment on the platform to winch.
+%   attachment on the platform to pulley.
 % 
 %   USEADVANCED: Optional positional parameter argument that tells the
 %   script to use advanced inverse kinematics algorithms to determining the
 %   cable length. Must be a logical value i.e, true or false
 %   
-%   'WinchOrientations': Matrix of orientations for each winch w.r.t. the
-%   global coordinate system. Every winch's orientation is stored in a
+%   'PulleyOrientations': Matrix of orientations for each pulley w.r.t. the
+%   global coordinate system. Every pulley's orientation is stored in a
 %   column where the first row is the orientation about x-axis, the second
 %   about y-axis, and the third about z-axis. This means, WINCHORIENTATIONS
 %   must be a matrix of 3xM values. The number of orientations, i.e., M,
-%   must match the number of winches in WINCHPOSITIONS (i.e., its column
-%   count) and the order must match the order winches in WINCHPOSITIONS
+%   must match the number of pulleyes in PULLEYPOSITIONS (i.e., its column
+%   count) and the order must match the order pulleyes in PULLEYPOSITIONS
 % 
-%   'WinchPulleyRadius': Vector of pulley radius per winch given in meter
+%   'PulleyRadius': Vector of pulley radius per pulley given in meter
 %   i.e., WINCHPULLEYRADIUS is a vector of 1xM values where M must match
-%   the number of winches in WINCHPOSITIONS and the order must be the same,
+%   the number of pulleyes in PULLEYPOSITIONS and the order must be the same,
 %   too.
 % 
 %   Outputs:
@@ -82,10 +93,10 @@ function [length, varargout] = inverseKinematics(Pose, WinchPositions, CableAtta
 %   determined using either simple or advanced kinematics
 %
 %   CABLEVECTOR: Vectors of each cable from attachment point to (corrected)
-%   winch point
+%   pulley point
 %   
 %   CABLEUNITVECTOR: Normalized vector for each cable from attachment point
-%   to its (corrected) winch point
+%   to its (corrected) pulley point
 % 
 % 
 % Author: Philipp Tempel <philipp.tempel@isw.uni-stuttgart.de>
@@ -106,53 +117,56 @@ valFcn_Pose = @(x) validateattributes(x, {'numeric'}, {'vector', 'ncols', 12}, m
 addRequired(ip, 'Pose', valFcn_Pose);
 
 % We need the a_i's ...
-valFcn_WinchPositions = @(x) validateattributes(x, {'numeric'}, {'2d', 'nrows', 3, 'ncols', size(CableAttachments, 2)}, mfilename, 'WinchPositions');
-addRequired(ip, 'WinchPositions', valFcn_WinchPositions);
+valFcn_PulleyPositions = @(x) validateattributes(x, {'numeric'}, {'2d', 'nrows', 3, 'ncols', size(CableAttachments, 2)}, mfilename, 'PulleyPositions');
+addRequired(ip, 'PulleyPositions', valFcn_PulleyPositions);
 
 % And we need the b_i's
-valFcn_CableAttachmens = @(x) validateattributes(x, {'numeric'}, {'2d', 'nrows', 3, 'ncols', size(WinchPositions, 2)}, mfilename, 'CableAttachments');
+valFcn_CableAttachmens = @(x) validateattributes(x, {'numeric'}, {'2d', 'nrows', 3, 'ncols', size(PulleyPositions, 2)}, mfilename, 'CableAttachments');
 addRequired(ip, 'CableAttachments', valFcn_CableAttachmens);
 
 % We allow the user to explicitley flag which algorithm to use
-valFcn_UseAdvanced = @(x) any(validatestring(x, {'yes', 'no', 'on', 'off'}, mfilename, 'UseAdvanced'));
-addOptional(ip, 'UseAdvanced', false, valFcn_UseAdvanced);
+valFcn_Algorithm = @(x) any(validatestring(x, {'standard', 'pulley', 'catenary'}, mfilename, 'UseAdvanced'));
+addOptional(ip, 'Algorithm', 'standard', valFcn_Algorithm);
 
-% We might want to use the winch orientations
-valFcn_WinchOrientations = @(x) validateattributes(x, {'numeric'}, {'2d', 'nrows', 3, 'ncols', size(WinchPositions, 2)}, mfilename, 'WinchOrientations');
-addParameter(ip, 'WinchOrientations', zeros(3, size(WinchPositions, 2)), valFcn_WinchOrientations);
+% We might want to use the pulley orientations
+valFcn_PulleyOrientations = @(x) validateattributes(x, {'numeric'}, {'2d', 'nrows', 3, 'ncols', size(PulleyPositions, 2)}, mfilename, 'PulleyOrientations');
+addParameter(ip, 'PulleyOrientations', zeros(3, size(PulleyPositions, 2)), valFcn_PulleyOrientations);
 
 % We might want the pulley radius to be defined if using advanced
 % kinematics
-valFcn_WinchPulleyRadius = @(x) validateattributes(x, {'numeric'}, {'vector', 'ncols', size(WinchPositions, 2)}, mfilename, 'WinchPulleyRadius');
-addParameter(ip, 'WinchPulleyRadius', zeros(1, size(WinchPositions, 2)), valFcn_WinchPulleyRadius);
+valFcn_PulleyRadius = @(x) validateattributes(x, {'numeric'}, {'vector', 'ncols', size(PulleyPositions, 2)}, mfilename, 'PulleyRadius');
+addParameter(ip, 'PulleyRadius', zeros(1, size(PulleyPositions, 2)), valFcn_PulleyRadius);
 
 % Configuratio nfor the input parser
 ip.KeepUnmatched = true;
 ip.FunctionName = mfilename;
 
 % Parse the provided inputs
-parse(ip, Pose, WinchPositions, CableAttachments, varargin{:});
+parse(ip, Pose, PulleyPositions, CableAttachments, varargin{:});
 
 
 
 %% Parse variables so we can use them natively
-bUseAdvanced = ip.Results.UseAdvanced;
+chAlgorithm = ip.Results.Algorithm;
 vPlatformPose = ip.Results.Pose;
-mWinchPositions = ip.Results.WinchPositions;
-mWinchOrientations = ip.Results.WinchOrientations;
-vWinchPulleyRadius = ip.Results.WinchPulleyRadius;
+mPulleyPositions = ip.Results.PulleyPositions;
+mPulleyOrientations = ip.Results.PulleyOrientations;
+vPulleyRadius = ip.Results.PulleyRadius;
 mCableAttachments = ip.Results.CableAttachments;
 
 
 
 %% Do the magic
 %%% What algorithm to use?
-% Advanced kinematics algorithm (including pulley radius)
-if bUseAdvanced
-    [vCableLength, mCableVector, mCableUnitVector, mWinchPulleyAngles] = algoInverseKinematics_Pulley(vPlatformPose, mWinchPositions, mCableAttachments, vWinchPulleyRadius, mWinchOrientations);
-% Simple kinematics algorithm (no pulley radius)
-else
-    [vCableLength, mCableVector, mCableUnitVector] = algoInverseKinematics_Simple(vPlatformPose, mWinchPositions, mCableAttachments);
+switch lower(chAlgorithm)
+    case 'catenary'
+%         [vCableLength, mCableVector, mCableUnitVector, mCableLine] = algoInverseKinematics_Catenary(vPlatformPose, mPulleyPositions, mCableAttachments, mPulleyOrientations, ?.?.?);
+    % Advanced kinematics algorithm (including pulley radius)
+    case 'pulley'
+        [vCableLength, mCableVector, mCableUnitVector, mPulleyAngles] = algoInverseKinematics_Pulley(vPlatformPose, mPulleyPositions, mCableAttachments, vPulleyRadius, mPulleyOrientations);
+    % Simple kinematics algorithm (no pulley radius)
+    case 'standard'
+        [vCableLength, mCableVector, mCableUnitVector] = algoInverseKinematics_Simple(vPlatformPose, mPulleyPositions, mCableAttachments);
 end
 
 
@@ -174,8 +188,8 @@ end
 % Fourth output argument...
 if nargout >= 4
     % For the advanced algorithms we are returning the wrapping angles
-    if bUseAdvanced
-        varargout{3} = mWinchPulleyAngles;
+    if chAlgorithm
+        varargout{3} = mPulleyAngles;
     end
 end
 
