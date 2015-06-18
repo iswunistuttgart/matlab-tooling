0e33aa1f90ede94225de25fb5a8f22ce0b0e7bc7
Philipp Tempel <mail@philipptempel.me>
2015-06-18 16:55:29 +0200

    Update issize to allow issize(A, [], c) and issize(A, r, [])

--- helpers/issize.m
+++ helpers/issize.m
@@ -7,27 +7,38 @@ function flag = issize(A, r, c)
 % 
 %   A: matrix to check for squareness
 %   
-%   r: rows matrix A has to have
+%   r: rows matrix A has to have. Can be empty to just check for the columns
+%   couunt to match
 %   
-%   c: number of columns matrix A has to have
+%   c: number of columns matrix A has to have. Can be empty to just check for
+%   the rows count to match
 % 
 %   Outputs:
 % 
 %   FLAG: evaluates to true if A is of size r x c, otherwise false
 % 
 % Author: Philipp Tempel <philipp.tempel@isw.uni-stuttgart.de>
-% Date: 2015-04-24
+% Date: 2015-06-18
 % Changelog:
-%   2015-04-24: Update validation function to use ```isequaln``` rather
-%               than ```isequal``` (slight improvement of readability and
-%               speed)
-%   2015-04-22: Initial release
+%   2015-06-18:
+%       * Update to allow for empty arguments so that we can just check the
+%       columns or rows count
+%   2015-04-24:
+%       * Update validation function to use ```isequaln``` rather than
+%       ```isequal``` (slight improvement of readability and speed)
+%   2015-04-22:
+%       * Initial release
 
 
 % ------------- BEGIN CODE --------------
 
-
-flag = ismatrix(A) && isequaln(size(A, 1), [r, c]);
+if isempty(r)
+    flag = ismatrix(A) && isequal(size(A, 2), c);
+elseif isempty(c)
+    flag = ismatrix(A) && isequal(size(A, 1), c);
+else
+    flag = ismatrix(A) && isequaln(size(A, 1), [r, c]);
+end
 
 
 end
