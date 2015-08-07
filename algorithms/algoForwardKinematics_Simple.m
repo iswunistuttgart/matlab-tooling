ba71a5e2c8121fe3e525f21e49af9e3ba9c3f1c1
Philipp Tempel <mail@philipptempel.me>
2015-08-07 16:25:39 +0200

    Update forward kinematics algorithm to a solution that seems to be working quite well

--- algorithms/algoForwardKinematics_Simple.m
+++ algorithms/algoForwardKinematics_Simple.m
@@ -73,10 +73,11 @@ vInitialStateForOptimization = algoForwardKinematics_PoseEstimate_Simple(vCableL
 opSolverOptions = optimoptions('lsqnonlin');
 opSolverOptions.Algorithm = 'levenberg-marquardt';
 opSolverOptions.Display = 'off';
-opSolverOptions.TolFun = 1e-10;
-opSolverOptions.TolX = 1e-12;
-% opSolverOptions.ScaleProblem = 'Jacobian';
-opSolverOptions.FinDiffType = 'central';
+opSolverOptions.TolFun = 1e-8;
+opSolverOptions.TolX = 1e-10;
+opSolverOptions.InitDamping = 1e-10;
+opSolverOptions.ScaleProblem = 'Jacobian';
+% opSolverOptions.FinDiffType = 'central';
 
 % Given any user-defined solver options? Process them now
 if ~isempty(stSolverOptionsGiven)
@@ -163,10 +164,12 @@ vEstimatedPose = [reshape(vPosition, 1, 3), rotationMatrixToRow(aRotation)];
 aJacobian = zeros(nNumberOfCables, 6);
 
 
+
 %% Calculate the cable length for the current pose estimate
 % Calculate the cable lengths for the estimated pose using the simple
 % inverse kinematics algorithm
 vLengths = algoInverseKinematics_Simple(vEstimatedPose, aPulleyPositions, aCableAttachments);
+
 
 
 %% And build the target optimization vector
@@ -184,34 +187,34 @@ if nargout > 1
     t5 = sin(vRotation(2));
     t6 = sin(vRotation(1));
     for iCable = 1:nNumberOfCables
-        t7 = t1 * t6;
-        t8 = t3 * t4;
-        t9 = t7 * t5 - t8;
-        t10 = t1 * t4;
-        t11 = t3 * t6;
-        t12 = t10 * t5 + t11;
-        t13 = t2 * aCableAttachments(1,iCable);
-        t14 = t13 * t1;
-        t15 = t9 * aCableAttachments(2,iCable);
-        t16 = t12 * aCableAttachments(3,iCable);
-        t17 = t15 + t16 + t14 - aPulleyPositions(1,iCable) + vPosition(1);
-        t10 = t11 * t5 + t10;
-        t7 = t8 * t5 - t7;
-        t8 = t13 * t3;
-        t11 = t10 * aCableAttachments(2,iCable) + t7 * aCableAttachments(3,iCable) + t8 - aPulleyPositions(2,iCable) + vPosition(2);
-        t18 = aCableAttachments(2,iCable) * t6;
-        t19 = aCableAttachments(3,iCable) * t4;
-        t20 = t19 + t18;
-        t21 = t5 * aCableAttachments(1,iCable);
-        t22 = t2 * t20 - t21 - aPulleyPositions(3,iCable) + vPosition(3);
-        t18 = -t2 * (t19 + t18) + t21;
+        t7 = t1*t6;
+        t8 = t3*t4;
+        t9 = t7*t5-t8;
+        t10 = t1*t4;
+        t11 = t3*t6;
+        t12 = t10*t5+t11;
+        t13 = t2*aCableAttachments(1,iCable);
+        t14 = t13*t1;
+        t15 = t9*aCableAttachments(2,iCable);
+        t16 = t12*aCableAttachments(3,iCable);
+        t17 = t15+t16+t14-aPulleyPositions(1,iCable)+vPosition(1);
+        t10 = t11*t5+t10;
+        t7 = t8*t5-t7;
+        t8 = t13*t3;
+        t11 = t10*aCableAttachments(2,iCable)+t7*aCableAttachments(3,iCable)+t8-aPulleyPositions(2,iCable)+vPosition(2);
+        t18 = aCableAttachments(2,iCable)*t6;
+        t19 = aCableAttachments(3,iCable)*t4;
+        t20 = t19+t18;
+        t21 = t5*aCableAttachments(1,iCable);
+        t22 = t2*t20-t21-aPulleyPositions(3,iCable)+vPosition(3);
+        t18 = -t2*(t19+t18)+t21;
         t19 = 0.2e1;
-        aJacobian(iCable,:) = [t19 * t17, ...
-                                t19 * t11, ...
-                                t19 * t22, ...
-                                t19 * (t17 * (t12 * aCableAttachments(2,iCable) - t9 * aCableAttachments(3,iCable)) + t11 * (-t10 * aCableAttachments(3,iCable) + t7 * aCableAttachments(2,iCable)) + t22 * t2 * (t4 * aCableAttachments(2,iCable) - t6 * aCableAttachments(3,iCable))), ...
-                                 -t19 * (t17 * t1 * t18 + t11 * t3 * t18 + t22 * (t20 * t5 + t13)), ...
-                                 -t19 * (-t11 * (t15 + t16 + t14) + t17 * (t10 * aCableAttachments(2,iCable) + t7 * aCableAttachments(3,iCable) + t8))];
+        aJacobian(iCable,:) = [t19*t17, ...
+                t19*t11, ...
+                t19*t22, ...
+                -t19*(t11*(t10*aCableAttachments(3,iCable)-t7*aCableAttachments(2,iCable))-t17*(t12*aCableAttachments(2,iCable)-t9*aCableAttachments(3,iCable))-t22*t2*(t4*aCableAttachments(2,iCable)-t6*aCableAttachments(3,iCable))), ...
+                -t19*(t17*t1*t18+t11*t3*t18+t22*(t20*t5+t13)), ...
+                -t19*(-t11*(t15+t16+t14)+t17*(t10*aCableAttachments(2,iCable)+t7*aCableAttachments(3,iCable)+t8))];
     end
 end
 
