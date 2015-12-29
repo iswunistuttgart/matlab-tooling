c96ddc9f705a274d5f0556502af59bd5937f2457
Philipp Tempel <mail@philipptempel.me>
2015-12-29 11:22:09 +0100

    Add signal generator for trapezoidal waves

---
+++ Implementation/helpers/signalGenerator_Trapezoid.m
@@ -0,0 +1,27 @@
+function [TrapezoidalValue, varargout] = signalGenerator_Trapezoid(CurrentTime, Amplitude, Period, Transition, Offset)
+
+if nargin < 2
+    Amplitude = 1;
+end
+
+if nargin < 3
+    Period = 2*pi;
+end
+
+if nargin < 4
+    Offset = 0;
+end
+
+dEvalTime = rem(CurrentTime, Period);
+
+TrapezoidalValue = Offset.*ones(1, numel(CurrentTime));
+
+TrapezoidalValue(dEvalTime >= 1/2*Period & dEvalTime < 1/2*Period + Transition) = Offset + Amplitude/Transition*(dEvalTime(dEvalTime >= 1/2*Period & dEvalTime < 1/2*Period + Transition) - 1/2*Period);
+
+% Amplitude/Transition*(dEvalTime(dEvalTime >= 1/2*Period & dEvalTime < 1/2*Period + Transition) - (1/2*Period + Transition)) + Offset;
+
+TrapezoidalValue(dEvalTime >= 1/2*Period + Transition & dEvalTime < Period - Transition) = Amplitude + Offset;
+
+TrapezoidalValue(dEvalTime >= Period - Transition & dEvalTime < Period) = Offset + Amplitude*(1 - 1/Transition*(dEvalTime(dEvalTime >= Period - Transition & dEvalTime < Period) - (Period - Transition)));
+
+end
