577f9cc6b5ed619d4091d822808f616a3bf2e87a
Philipp Tempel <mail@philipptempel.me>
2016-04-13 12:21:13 +0200

    Add folder 'csv' to startup.m

--- startup.m
+++ startup.m
@@ -6,6 +6,7 @@ cePaths = {
     fullfile(chPath, '..', '..', 'Matlab', 'Helper');
     fullfile(chPath, 'Implementation');
     fullfile(chPath, 'Implementation', 'algorithms');
+    fullfile(chPath, 'Implementation', 'csv');
     genpath(fullfile(chPath, 'Implementation', 'helpers'));
     fullfile(chPath, 'Implementation', 'mat');
     fullfile(chPath, 'Implementation', 'robots');
