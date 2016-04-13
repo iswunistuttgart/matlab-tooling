76c8709bfb3c68d30089d5faa7ddc2da980d7461
Philipp Tempel <mail@philipptempel.me>
2016-04-13 12:17:50 +0200

    Update vec2skew to support matrix input and output

--- Implementation/utils/math/vec2skew.m
+++ Implementation/utils/math/vec2skew.m
@@ -1,20 +1,36 @@
 function skew = vec2skew(vec)%#codegen
-% VEC2SKEW Turn a vector into its skew-symmetric matrix form
+% VEC2SKEW Turn the input into its skew-symmetrix matrix form
 % 
-%   VEC2SKEW(VEC) turns vec into its skew-symmetric matrix form of type
+%   SKEW = VEC2SKEW(VEC) turns vec into its skew-symmetric matrix form of type
 %   [0,      -vec(3),  vec(2); ...
 %    vec(3),  0,      -vec(1); ...
 %   -vec(2),  vec(1),  0];
+%   If VEC is a Mx3 matrix, then SKEW is a 3x3xM matrix of skew-symmetrix
+%   matrices for each row of VEC concatenated along the third dimension
 %   
 %   
 %   Inputs:
 %   
-%   VEC: a vector of length 3
+%   VEC: Matrix of column size 3. If VEC is Mx3 matrix, output is a 3x3xM
+%   matrix.
 %
 %   Outputs:
 %   
-%   SKEW: Skew-symmetric matrix of VEC
+%   SKEW: Skew-symmetric matrix of VEC. If VEC is a Mx3 matric, SKEW is 3x3xM.
 %
+
+
+
+%% File information
+% Author: Philipp Tempel <philipp.tempel@isw.uni-stuttgart.de>
+% Date: 2016-04-12
+% Changelog:
+%   2016-04-12
+%       * Allow vec2skew to work on matrices thanks to help from quat2rot.m
+%       * Update help doc
+%       * Add block "File information"
+%   2015-04-24
+%       * Initial release
 
 
 
@@ -22,15 +38,32 @@ function skew = vec2skew(vec)%#codegen
 % Gotta have doubles
 assert(isa(vec, 'double') || isa(vec, 'sym'), 'Input must be double or symbolic.');
 % Gotta have at least three elements inside VEC
-assert(numel(vec) == 3, 'Input must be only three elements long.');
+assert(size(vec, 2) == 3, 'Input must have three columns.');
 
 
 
-%% Magic
-% Create skew symmetric matrix
-skew = [0,      -vec(3),    vec(2); ...
-        vec(3),  0,         -vec(1); ...
-        -vec(2), vec(1),    0];
+%% Process inputs
+% Get all vectors for wich to get the skew-symmetric matrix
+aVectors = vec.';
+% Number of vectors == cols of aVectors
+nVectors = size(aVectors, 2);
+% Reshape the vector in the depth dimension
+aVectors = reshape(aVectors, [3, 1, nVectors]);
+
+% Quicker access to important parts
+s = zeros(1, 1, nVectors);
+x = aVectors(1,1,:);
+y = aVectors(2,1,:);
+z = aVectors(3,1,:);
+
+
+
+%% Magic (copied from quat2rotm)
+
+% Explicitly define concatenation dimension for codegen
+tempS = cat(1, s, -z, y, z, s, -x, -y, x, s);
+skew = reshape(tempS, [3, 3, nVectors]);
+skew = permute(skew, [2, 1, 3]);
 
 
 end
