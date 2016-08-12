85c6bc5b6e6cda59454a5f3ed4547105b70db1a1
Philipp Tempel <mail@philipptempel.me>
2016-08-12 10:18:27 +0200

    Update setfigureratio to better input argument handling

--- Implementation/utils/plot/setfigureratio.m
+++ Implementation/utils/plot/setfigureratio.m
@@ -1,7 +1,8 @@
 function setfigureratio(Ratio, varargin)
 % SETFIGURERATIO sets the ratio of the current or given figure
 %
-%   SETFIGURERATIO(RATIO) sets the figure to the chosen width:height ratio.
+%   SETFIGURERATIO(RATIO) sets the ratio of the currently active figure to the
+%   chosen width:height ratio.
 %
 %   SETFIGURERATIO(FIG, ...) sets ratio RATIO on the given figure.
 %
@@ -18,21 +19,21 @@ function setfigureratio(Ratio, varargin)
 
 %% File information
 % Author: Philipp Tempel <philipp.tempel@isw.uni-stuttgart.de>
-% Date: 2016-08-02
+% Date: 2016-08-12
 % Changelog:
+%   2016-08-12
+%       * Minor fixes to how input arguments are handled
 %   2016-08-02
 %       * Add help doc block and update other doc blocks
-%       * Update to using ```newplot``` and ```acestor```
+%       * Update to using ```newplot``` and ```ancestor```
 %   2016-04-01
 %       * Initial release
 
 
 
-%% Pre-process inputs
-% Target figure
+%% Pre-process inputs (shift figure)
 hfTarget = [];
-% Allow the target figure to be given as the first argument
-if ~isempty(varargin) && isa(Ratio, 'matlab.ui.Figure')
+if ~isempty(varargin) && isfig(Ratio)
     hfTarget = Ratio;
     Ratio = varargin{1};
     varargin = varargin(2:end);
@@ -40,26 +41,37 @@ end
 
 
 
+%% Define the input parser
+ip = inputParser;
 
-%% Assertion
-assert(isa(Ratio, 'char'), 'Ratio must be a char');
+% Require: Anchors. Must be a 3xN array
+valFcn_Ratio = @(x) validateattributes(x, {'char'}, {'nonempty'}, mfilename, 'Ratio');
+addRequired(ip, 'Ratio', valFcn_Ratio);
+
+% Configuration of input parser
+ip.KeepUnmatched = true;
+ip.FunctionName = mfilename;
+
+% Parse the provided inputs
+try
+    args = [{Ratio}, varargin];
+    
+    parse(ip, args{:});
+catch me
+    throwAsCaller(MException(me.identifier, me.message));
+end
 
 
 
 %% Parse arguments
+% Get the target figure
 if isempty(hfTarget)
-    % Target axes
-    haTarget = newplot();
-    hfTarget = ancestor(haTarget,'figure');
-end
-% Ratio
-chRatio = Ratio;
-% Parse the string to find either 'width' or 'width:height'
-stRatio = regexp(chRatio, '(?<width>\d+):(?<height>\d+)', 'names');
-% If no figure given, we will fallback to the active one
-if ~ishandle(hfTarget)
     hfTarget = gcf;
 end
+% Ratio
+chRatio = ip.Results.Ratio;
+% Parse the string to find either 'width' or 'width:height'
+stRatio = regexp(chRatio, '(?<width>\d+):(?<height>\d+)', 'names');
 
 % Ensure we have at least a width given
 if ~isfield(stRatio, 'width')
@@ -77,7 +89,7 @@ stRatio.width = str2double(stRatio.width);
 
 % Get the current figures position (thus position and width)
 vPosition = get(hfTarget, 'Position');
-vWidth = vPosition(3);
+% vWidth = vPosition(3);
 vHeight = vPosition(4);
 
 % Set the new height
@@ -90,6 +102,9 @@ vNewPosition = [vPosition(1), vPosition(2), vNewWidth, vNewHeight];
 
 % And set the position (thus location and dimensions) on the figure;
 set(hfTarget, 'Position', vNewPosition);
+
+% Finally, make sure the figure is drawn
+drawnow
 
 
 end
