f8416ffc5d82b0ef6f581848dda27d934318823e
Philipp Tempel <mail@philipptempel.me>
2016-09-06 09:28:35 +0200

    Add two helpers to save and load robot config

---
+++ Implementation/helpers/robots/robload.m
@@ -0,0 +1,92 @@
+function [varargout] = robload(Name, varargin)
+% ROBLOAD loads a given robot configuration
+%
+%   Inputs:
+%
+%   NAME        Name of robot to load
+
+
+
+%% File information
+% Author: Philipp Tempel <philipp.tempel@isw.uni-stuttgart.de>
+% Date: 2016-09-06
+% Changelog:
+%   2016-09-06
+%       * Initial release
+
+
+
+%% Define the input parser
+ip = inputParser;
+
+% Require: Anchors. Must be a 3xN array
+valFcn_Name = @(x) validateattributes(x, {'char'}, {'nonempty'}, mfilename, 'Name');
+addRequired(ip, 'Name', valFcn_Name);
+
+% Configuration of input parser
+ip.KeepUnmatched = true;
+ip.FunctionName = mfilename;
+
+% Parse the provided inputs
+try
+    varargin = [{Name}, varargin];
+    
+    parse(ip, varargin{:});
+catch me
+    throwAsCaller(MException(me.identifier, me.message));
+end
+
+
+
+%% Parse IP variables and create local variables
+% Name of robot to load
+chName = ip.Results.Name;
+% Path of this file
+[chFile_Path, ~, ~] = fileparts(mfilename('fullpath'));
+% Two levels up and down into folder 'robots' is where we will be storing the
+% data
+chPath_Store = fullpath(fullfile(chFile_Path, '..', '..', 'robots'));
+
+
+
+%% Find the robot file
+% Find files inside the robot storage that match the robot name
+stFiles = dir(fullfile(chPath_Store, sprintf('%s*', chName)));
+
+% Ensure we found files
+assert(~isempty(stFiles), 'PHILIPPTEMPEL:ROBLOAD:noRobotFound', 'No robot configuration found for robot [%s].', chName);
+
+% Sort the files from newest to oldest
+[~, nSortedIdx] = sort([stFiles.datenum], 'descend');
+
+% Get the newest file
+iFile = nSortedIdx(1);
+
+% Load the data file
+stData = load(fullfile(chPath_Store, stFiles(iFile).name));
+
+
+
+%% Assign output quantities
+% If no output is given, we will assign all variables in the caller's workspace
+if nargout == 0
+    % Get variable names
+    ceVariableNames = fieldnames(stData);
+    % Loop over all variables and assign them in the caller's workspace
+    for iData = 1:numel(ceVariableNames)
+        assignin('caller', ceVariableNames{iData}, stData.(ceVariableNames{iData}));
+    end
+end
+
+% If one output, we will assign the loaded struct to it
+if nargout < 2
+    varargout{1} = stData;
+end
+
+
+end
+
+%------------- END OF CODE --------------
+% Please send suggestions for improvement of this file to the original author as
+% can be found in the header Your contribution towards improving this function
+% will be acknowledged in the "Changes" section of the header
---
+++ Implementation/helpers/robots/robsave.m
@@ -0,0 +1,151 @@
+function [] = robsave(varargin)
+% ROBSAVE saves a robot configuration to the storage of all robots
+%
+%   ROBSAVE saves the configruation variables found in the workspace under the
+%   name found in variable 'Robot_Name' in the global robots storage folder of
+%   this matlab tooling kit.
+%
+%   ROBSAVE(NAME) saves configuration variables found in the workspace under
+%   name NAME.
+%
+%   ROBSAVE(NAME, VARIABLES) saves only configuration variables listed in cell
+%   array VARIABLES.
+%
+%   ROBSAVE(NAME, VARIABLES, OVERWRITE) sets the overwrite toggle to OVERWRITE
+%   which can be any of the following list
+%       'on', 'yes'     Overwrites target file
+%       'off', 'no'     Does not overwrite target file
+%   If a target file is found and OVERWRITE is set to 'off' or 'no', the current
+%   timestamp will be appended to the file.
+%
+%   ROBSAVE('Name', 'Value', ...) uses name-value pair syntax.
+%
+%   Optional Inputs -- specified as parameter value pairs
+%   Name        Name of the robot to save current configuration as
+%
+%   Variables   If variables should not be taken from the workspace but given by
+%               the user, this cell array must contain the variables' names.
+%
+%   Overwrite   Switch to enable or disable overwriting. Possible values are
+%               'on', 'yes'     Overwrite target file
+%               'off', 'no'     Do not overwrite target file but append current
+%                               timestamp to the filename
+
+
+
+%% File information
+% Author: Philipp Tempel <philipp.tempel@isw.uni-stuttgart.de>
+% Date: 2016-09-06
+% Changelog:
+%   2016-09-06
+%       * Initial release
+
+
+
+%% Define the input parser
+ip = inputParser;
+
+% Require: Anchors. Must be a 3xN array
+valFcn_Name = @(x) validateattributes(x, {'char'}, {'nonempty'}, mfilename, 'Name');
+addParameter(ip, 'Name', '', valFcn_Name);
+
+% Optional 1: AnchorSpec. One-dimensional or two-dimensional cell-array
+valFcn_Variables = @(x) validateattributes(x, {'struct'}, {}, mfilename, 'Variables');
+addParameter(ip, 'Variables', {}, valFcn_Variables);
+
+% Overwrite: Char. Matches {'on', 'off', 'yes', 'no'}. Defaults 'no';
+valFcn_Overwrite = @(x) any(validatestring(x, {'on', 'off', 'yes', 'no'}, mfilename, 'Overwrite'));
+addParameter(ip, 'Overwrite', 'off', valFcn_Overwrite);
+
+% Configuration of input parser
+ip.KeepUnmatched = true;
+ip.FunctionName = mfilename;
+
+% Parse the provided inputs
+try
+    parse(ip, varargin{:});
+catch me
+    throwAsCaller(MException(me.identifier, me.message));
+end
+
+
+
+%% Parse IP variables and create other, local variables
+
+% Name of robot
+chName = ip.Results.Name;
+% Variables to save
+stVariables = ip.Results.Variables;
+% Overwrite toggle
+chOverwrite = ip.Results.Overwrite;
+% Path of this file
+[chFile_Path, ~, ~] = fileparts(mfilename('fullpath'));
+% Two levels up and down into folder 'robots' is where we will be storing the
+% data
+chPath_Store = fullpath(fullfile(chFile_Path, '..', '..', 'robots'));
+
+% Ensure target folder exists
+if ~exist(chPath_Store, 'dir')
+    mkdir(chPath_Store);
+end
+% Ensure we have a name of the robot
+if isempty(chName)
+    % Get robot name from workspace stored in variable 'Robot_Name'
+    stRobotName = evalin('caller', 'whos(''Robot_Name'');');
+    % If we have found some variable, we only need to parse it
+    if ~isempty(stRobotName)
+        chName = evalin('caller', stRobotName(1).name);
+    end
+end
+% Ensure we have variables
+if isempty(stVariables)
+    % Get all variables in workspace
+    stVariables = evalin('base', 'whos(''*'');');
+    % And get the names of all variables
+    stVariables = {stVariables(:).name};
+end
+
+% Filename of stored file
+chFilename = fullfile(chPath_Store, chName);
+% Append to MAT file name
+chFile_Appendix = '';
+
+% Ensure we are not overwriting the target file
+if strcmp(chOverwrite, 'off')
+    if 2 == exist([chFilename , '.mat'], 'file')
+        chFile_Appendix = sprintf('_%s', datestr8601(now));
+    end
+end
+
+% And finally, append the appendix to the filename
+chFilename = [chFilename , chFile_Appendix];
+
+
+
+%% Additional assertion
+
+% We need variables to save...
+assert(~isempty(stVariables), 'PHILIPPTEMPEL:ROBSAVE:noVariablesFound', 'No variables given and no variables found in caller workspace');
+
+
+
+%% Do your code magic here
+
+% Keeps a copy of each variable to save later on
+stStore = struct();
+
+% Loop over all variables and store them to a local struct
+for iVar = 1:numel(stVariables)
+    stStore.(stVariables{iVar}) = evalin('caller', stVariables{iVar});
+end
+
+% And finally, save the file from the struct we have created above
+save(chFilename, '-struct', 'stStore');
+
+
+end
+
+%------------- END OF CODE --------------
+% Please send suggestions for improvement of this file to the original author as
+% can be found in the header Your contribution towards improving this function
+% will be acknowledged in the "Changes" section of the header
