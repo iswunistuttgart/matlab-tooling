ab8460ff36cd59ddf3042983168680d9d606ed5e
Philipp Tempel <mail@philipptempel.me>
2016-09-09 14:56:34 +0200

    Nicer code of tsave so it will work with already given file extensions

--- Implementation/utils/sys/tsave.m
+++ Implementation/utils/sys/tsave.m
@@ -9,8 +9,11 @@ function [] = tsave(Filename, varargin)
 
 %% File information
 % Author: Philipp Tempel <philipp.tempel@isw.uni-stuttgart.de>
-% Date: 2016-09-01
+% Date: 2016-09-09
 % Changelog:
+%   2016-09-09
+%       * Update script so that it will work with file extension given in the
+%       file name
 %   2016-09-01
 %       * Initial release
 
@@ -26,13 +29,10 @@ assert(~isempty(Filename), 'PHILIPPTEMPEL:TSAVE:emptyFilename', 'Filename cannot
 %% Assign local variables
 % Date format to append to filename
 chDateFormat = 'yyyymmdd_HHMMSSFFF';
-% Get filename and adjust
+% % Get filename and adjust
 chFilename = Filename;
-if ~strcmp(chFilename(end), '_')
-    chFilename = [chFilename , '_'];
-end
-% Append timestamp to filename
-chFilename = sprintf('%s%s', chFilename, datestr(datevec(now), chDateFormat));
+% Did we create a backup?
+
 
 
 %% Magic!
@@ -44,11 +44,33 @@ if ~isempty(varargin)
     % appended by "'"
     chArgs = sprintf(', ''%s''', strjoin(varargin, ''', '''));
 end
-% Create the command
-chCommand = sprintf('save(''%s''%s);', chFilename, chArgs);
 
-% Evaluate the command in the caller's workspace
-evalin('caller', chCommand);
+% Get the file path, file name, and extension of the file to save
+[chFile_Path, chFile_Name, chFile_Ext] = fileparts(chFilename);
+
+% Give the default '.mat' file extension to saving files
+if isempty(chFile_Ext)
+    chFile_Ext = '.mat';
+end
+
+% Append an underscore to the actual file name
+if ~strcmp(chFile_Name(end), '_')
+    chFile_Name = [chFile_Name , '_'];
+end
+
+% Just to avoid having to type this over and over again
+chFile_Fullname = fullfile(chFile_Path, sprintf('%s%s%s', chFile_Name, datestr(now, chDateFormat), chFile_Ext));
+
+% Create the command
+chCommand = sprintf('save(''%s''%s);', chFile_Fullname, chArgs);
+
+% Try to save the file
+try
+    % Evaluate the command in the caller's workspace
+    evalin('caller', chCommand);
+catch me
+    throwAsCaller(me);
+end
 
 
 
