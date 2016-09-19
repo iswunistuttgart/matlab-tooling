53274dd5b6a0ea75bf91500607fb1d7c01889234
Philipp Tempel <mail@philipptempel.mde>
2016-09-19 20:35:39 +0200

    Remove files now parted to CDPR-Toolbox

--- Implementation/utils/experiments/cogiro_eul2rotm.m
+++
@@ -1,5 +0,0 @@
-function R = cogiro_eul2rotm(eul, varargin)
-
-R = transpose(eul2rotm(-eul, varargin{:}));
-
-end
--- startup.m
+++ startup.m
@@ -3,15 +3,7 @@ function startup
 chPath = fileparts(mfilename('fullpath'));
 
 cePaths = {
-    fullfile(chPath, '..', '..', 'Matlab', 'Helper');
-    fullfile(chPath, 'Data');
-    fullfile(chPath, 'Data', 'robots');
-    fullfile(chPath, 'Data', 'workspace');
     fullfile(chPath, 'Implementation');
-    fullfile(chPath, 'Implementation', 'algorithms');
-    fullfile(chPath, 'Implementation', 'csv');
-    genpath(fullfile(chPath, 'Implementation', 'helpers'));
-    fullfile(chPath, 'Implementation', 'mat');
     genpath(fullfile(chPath, 'Implementation', 'utils'));
 };
 
