fef515fb7ac078f3789bcc12ffa7ce71807d2e03
Philipp Tempel <mail@philipptempel.me>
2016-09-20 07:37:09 +0200

    Add help block to siggen_rectangular

--- Implementation/utils/signal/siggen_rectangular.m
+++ Implementation/utils/signal/siggen_rectangular.m
@@ -1,4 +1,25 @@
-function [RectangularValue, varargout] = siggen_rectangular(CurrentTime, Amplitude, Period, Offset)
+function [RectangularValue, varargout] = sigen_rectangular(CurrentTime, Amplitude, Period, Offset)
+% SIGEN_RECTANGULAR generates a rectangular signal
+%
+%   SIG = SIGEN_RECTANGULAR() generates a rectangular signal over time
+%   t = [0, 1] with amplitude 1 and rising edge at t = 0.5.
+%
+%   SIG = SIGEN_RECTANGULAR(TIME) generates a rectangular signal over
+%   time TIME with amplitude 1 and rising edge at t = TIME/2.
+%
+%   SIG = SIGEN_RECTANGULAR(TIME, 'Name', 'Value') generates a
+%   rectangular signal signal with optional parameters specified as name-value
+%   pairs.
+%
+%   Inputs:
+%
+%   TIME    Time at which to evaluate
+%
+%   Optional Inputs -- specified as parameter value pairs
+%
+%   Offset
+%
+%   
 
 if nargin < 2
     Amplitude = 1;
