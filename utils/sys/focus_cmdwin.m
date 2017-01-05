55206f6d011255de91400b5aaba72300cb4eb9dc
Philipp Tempel <mail@philipptempel.me>
2017-01-05 15:12:12 +0100

    Add helper to give focus to the command window

---
+++ utils/sys/focus_cmdwin.m
@@ -0,0 +1,25 @@
+function focus_cmdwin()
+% FOCUS_CMDWIN gives focus to the command window
+
+
+
+%% File information
+% Author: Philipp Tempel <philipp.tempel@isw.uni-stuttgart.de>
+% Date: 2017-01-05
+% Changelog:
+%   2017-01-05
+%       * Initial release
+
+
+
+%% Do your code magic here
+
+commandwindow
+
+
+end
+
+%------------- END OF CODE --------------
+% Please send suggestions for improvement of this file to the original author as
+% can be found in the header Your contribution towards improving this function
+% will be acknowledged in the "Changes" section of the header
