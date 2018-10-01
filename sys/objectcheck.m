1a372cfdb1586b08d507ab3a51ead2c776dde5c8
Philipp Tempel <mail@philipptempel.me>
2018-10-01 14:34:38 +0200

    Change behavior of `objectcheck` to work with arrays of objects, too

--- sys/objectcheck.m
+++ sys/objectcheck.m
@@ -1,17 +1,20 @@
 function [obj, args, nargs] = objectcheck(oc, varargin)
 % OBJECTCHECK checks for an object of a calling class in the list of arguments
 %
+%   [OBJ, ARGS, NARGS] = OBJECTCHECK(OC, ...) checks of an object of class OC in
+%   the given list of variable arguments.
+%
 %   Inputs:
 %
 %   OC                  String of class of object to find.
 %
 %   Outputs:
 %
-%   OBJ                 Description of argument OBJ
+%   OBJ                 First object of type OC that is found, otherwise empty.
 %
-%   ARGS                Description of argument ARGS
+%   ARGS                Arguments remaining that are not of type OC.
 %
-%   NARGS               Description of argument NARGS
+%   NARGS               Number of remaining arguments not of type OC.
 
 
 
@@ -27,13 +30,17 @@ function [obj, args, nargs] = objectcheck(oc, varargin)
 %% Valdiate arguments
 try
   % OBJECTCHECK(O, ARG)
+  % OBJECTCHECK(O)
   % OBJECTCHECK(O, ARG1, ...)
-  narginchk(2, Inf);
+  narginchk(1, Inf);
   % OBJECTCHECK(...)
   % OBJ = OBJECTCHECK(...)
   % [OBJ, ARGS] = OBJECTCHECK(...)
   % [OBJ, ARGS, NARGS] = OBJECTCHECK(...)
   nargoutchk(0, 3);
+  
+  % Make sure the object given is a string
+  validateattributes(oc, {'char'}, {'nonempty'}, mfilename, 'OC');
 catch me
   throwAsCaller(me);
 end
@@ -47,25 +54,25 @@ args = varargin;
 nargs = (nargin - 1);
 obj = [];
 
+% Do we have any arguments and is the first one already our sought fro object?
 if nargs > 0 && ( isscalar(args{1}) && isa(args{1}, oc) )
   obj = args{1};
   args = args(2:end);
   nargs = nargs - 1;
 end
 
+% Process remaining arguments
 if nargs > 0
-  inds = find(strcmpi('parent', args) );
+  
+  % Find where this an object matching in class
+  inds = find(cellfun(@(cc) isa(cc, oc), args));
+  
+  % If there were any finds
   if ~isempty(inds)
-    inds = unique([inds inds+1]);
     pind = inds(end);
     
-    % Check for either a scalar handle, or any size array of graphics objects.
-    % If the argument is passed using the 'Parent' P/V pair, then we will
-    % catch any graphics handle(s), and not just Axes.
-    if nargs >= pind && ...
-            ((isscalar(args{pind}) && isgraphics(args{pind})) ...
-            || isa(args{pind},'matlab.graphics.Graphics'))
-      ax = handle(args{pind});
+    if nargs > pind && isa(args{pind}, oc)
+      obj = args{pind};
       args(inds) = [];
       nargs = length(args);
     end
