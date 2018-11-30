3438b15ea1cdd7ca1b5c075bf4a41eca5a9068a0
Philipp Tempel <mail@philipptempel.me>
2018-11-30 19:31:26 +0100

    New Contents.m files

--- signal/Contents.m
+++ signal/Contents.m
@@ -1,6 +1,7 @@
 % SIGNAL
 %
 % Files
+%   bump               - Bump function
 %   gauss              - calculates the 1D gaussian function along X
 %   h2r                - converts Hertz to radian per second
 %   r2h                - converts radian per second to Hertz
@@ -8,3 +9,4 @@
 %   siggen_rectangular - generates a rectangular signal
 %   siggen_sawtooth    - 
 %   siggen_trapezoid   - 
+%   smoothtrans        - Evaluate a smooth transition function at value X
--- sys/Contents.m
+++ sys/Contents.m
@@ -1,6 +1,7 @@
 % SYS
 %
 % Files
+%   argstring                   - truns a list of arguments into a human-readable string
 %   bytes2str                   - turns the number of bytes into a human readable string
 %   computername                - returns the name of the computer in the local network
 %   cprintf                     - displays styled formatted text in the Command Window
@@ -14,12 +15,15 @@
 %   iif                         - Allows conditionals in inline and anonymous functions
 %   isinpath                    - Checks whether the given path is part of MATLAB's environment path
 %   matlab_logo                 - 
+%   objectcheck                 - checks for an object of a calling class in the list of arguments
 %   pack_files_and_dependencies - packs dependent files into one folder
 %   progressbar                 - creates a nicer progress bar window for progress information
 %   restart                     - executes a few functions to reset MATLAB workspace
+%   rgb                         - converts a conventional RGB representation into MATLAB RGB format
 %   rmpaths                     - Remove directory from search path recursively
 %   save_figure                 - Saves the figure under the specified filename with variable types
 %   save_ws                     - saves the whole workspace to a unique filename 
+%   secs2hms                    - Convert seconds to a human readable time format
 %   setrng                      - sets the random number generator to a set or pre-defined options
 %   stopalltimers               - stops all timers whether they are visible or not
 %   tsave                       - 
